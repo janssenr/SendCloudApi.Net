bc985b44d375e85324b05a10dff2de8f1d544552
Rob Janssen <rob.janssen@upcmail.nl>
2021-08-25 11:23:57 +0200

    Set IsRequired to false for Country fields

--- src/SendCloudApi.Net/Models/Country.cs
+++ src/SendCloudApi.Net/Models/Country.cs
@@ -20,16 +20,16 @@ namespace SendCloudApi.Net.Models
         [DataMember(Name = "iso_3", EmitDefaultValue = false, IsRequired = true)]
         public string Iso3 { get; set; }
 
-        [DataMember(Name = "from_id", EmitDefaultValue = false, IsRequired = true)]
+        [DataMember(Name = "from_id", EmitDefaultValue = false, IsRequired = false)]
         public int FromId { get; set; }
 
-        [DataMember(Name = "from_name", EmitDefaultValue = false, IsRequired = true)]
+        [DataMember(Name = "from_name", EmitDefaultValue = false, IsRequired = false)]
         public string FromName { get; set; }
 
-        [DataMember(Name = "from_iso_2", EmitDefaultValue = false, IsRequired = true)]
+        [DataMember(Name = "from_iso_2", EmitDefaultValue = false, IsRequired = false)]
         public string FromIso2 { get; set; }
 
-        [DataMember(Name = "from_iso_3", EmitDefaultValue = false, IsRequired = true)]
+        [DataMember(Name = "from_iso_3", EmitDefaultValue = false, IsRequired = false)]
         public string FromIso3 { get; set; }
     }
 }
